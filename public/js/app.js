9ef79c71be37608ba2730634275e5a3a75fa585f
Ryan Newton <rnewton@hearsaycorp.com>
2015-12-25 14:30:12 -0500

    Clean up a lot more

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,33 @@
-require(['./placeHolder'], function(placeHolder) {
-
+require([
+	'./views/trips_home_view',
+	'./views/trip_individual_view'
+], function(
+	TripsHomeView,
+	TripIndividualView
+	
+) {
 	$(document).ready(function() {
-		// Clashes with erb otherwise
-		_.templateSettings = {
-		    interpolate: /\{\{\=(.+?)\}\}/g,
-		    evaluate: /\{\{(.+?)\}\}/g
-		};
 
-
-		placeHolder();
-
-
+		var LocationTrackerRouter = Backbone.Router.extend({
+		    routes: {
+		        "trips/:id": "trip",
+		        "": "index"
+		    },
+		    index: function() {
+				var tripsHomeView = new TripsHomeView({
+					el: $('#main-container')
+				});
+		    },
+		    trip: function(id) {
+				var tripIndividualView = new TripIndividualView({
+					el: $('#main-container'),
+					id: id
+				});
+		    },
+		});
+	
+		new LocationTrackerRouter();
+	
+		Backbone.history.start();
 	});
 });
